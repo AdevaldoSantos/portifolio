5c0911722b2507d446e9b30dc340c7399a490b3b
Adevaldo Santos <adevaldosantosfilho@gmail.com>
2024-02-07 09:56:58 -0300

    Adicionando funções de mudar a imagem e restaurar a imagem no arquivo foto.js

---
+++ foto.js
@@ -0,0 +1,19 @@
+const perfil = document.querySelector('img')
+
+perfil.onmouseout = () => restaurarImg()
+perfil.onmouseover = () => mudarImg()
+
+function restaurarImg() {
+    perfil.style.transition = "all 1s"
+    perfil.style.width = ""
+    perfil.style. boxShadow = ""
+    perfil.style.margin = "0px"
+}
+
+function mudarImg() {
+    perfil.style.transition = "all 1s"
+    perfil.style.width = "40%"
+    perfil.style.boxShadow = "0px 0px 20px 20px #2d385c"
+    perfil.style.margin = "30px"
+}
+
