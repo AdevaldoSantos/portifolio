9712cb8322426bbb5a68eb569869be04aa42e2a2
Adevaldo Santos <adevaldosantosfilho@gmail.com>
2024-02-12 18:13:50 -0300

    adicionando uma section e imagens para fazer o campo linguans de programação

--- foto.js
+++ foto.js
@@ -12,7 +12,7 @@ function restaurarImg() {
 
 function mudarImg() {
     perfil.style.transition = "all 1s"
-    perfil.style.width = "40%"
+    perfil.style.width = "30%"
     perfil.style.boxShadow = "0px 0px 20px 20px #2d385c"
     perfil.style.margin = "30px"
 }
