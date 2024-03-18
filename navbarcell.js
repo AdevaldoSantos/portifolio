82b506f3d111a1222a662cbb8da983230d864236
Adevaldo Santos <adevaldosantosfilho@gmail.com>
2024-03-18 13:11:10 -0300

    adicionando o navbarCell

--- navbarcell.js
+++ navbarcell.js
@@ -1,12 +1,18 @@
 function verificadorLarguraDaTela() {
     let larguraDaTela = window.innerWidth;
     let nav = document.getElementById("nav")
+    let navCell = document.getElementById("navCell")
+    let h1 = document.getElementById("h1")
 
     if (larguraDaTela <= 880) {
         nav.style.display = "none"
+        navCell.style.display = "flex"
+        h1.style.marginTop = "70px"
+        
     } 
     else {
         nav.style.display = "flex"
+        navCell.style.display = "none"
     }
 }
 
